0c24d85234217afd95aa13f48d808aa567f9def8
ETLDevs <etlbdevs@gmail.com>
2023-01-07 21:16:11 +0200

    createNewItem function & queried productList

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const userInput = document.querySelector('.userInput');
+const productList = document.querySelector('.productList');
+
+const createNewItem = (product) => {
+    const newItem = document.createElement('li');
+    newItem.innerHTML = product;
+    productList.appendChild(newItem);
+}
 
 document.querySelector('.insertProductBtn').addEventListener('click', () => {
 const product = userInput.value;
-
+createNewItem(product)
 })
