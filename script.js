d35ebdaec72e806a28a5a333a06a2257404be0b0
ETLDevs <etlbdevs@gmail.com>
2023-01-07 21:54:03 +0200

    add itemsCounter, data atrr to newItem

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const userInput = document.querySelector('.userInput');
 const productList = document.querySelector('.productList');
+let itemsCounter = 0;
 
 const createNewItem = (product) => {
     const newItem = document.createElement('li');
     newItem.innerHTML = product;
+    newItem.dataset.item = itemsCounter;
     productList.appendChild(newItem);
 }
 
 document.querySelector('.insertProductBtn').addEventListener('click', () => {
 const product = userInput.value;
+itemsCounter++;
 createNewItem(product);
 userInput.value = '';
 userInput.focus();
