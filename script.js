bc59ebc933c1170bf4c019688cb19373412bd696
ETLDevs <etlbdevs@gmail.com>
2023-01-07 21:55:10 +0200

    add function addItemsFeatures

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ const createNewItem = (product) => {
     newItem.innerHTML = product;
     newItem.dataset.item = itemsCounter;
     productList.appendChild(newItem);
+    addItemsFeatures(newItem);
+}
+
+
+const addItemsFeatures = (newItem) => {
+    const quantity = document.createElement('input');
+    quantity.type = 'number';
+    quantity.placeholder = 'Qty'
+    const comments = document.createElement('input');
+    comments.placeholder = 'Comments';
+    const remove = document.createElement('button');
+    remove.innerHTML = 'REMOVE';
+    newItem.append(quantity, comments, remove)
 }
 
 document.querySelector('.insertProductBtn').addEventListener('click', () => {
