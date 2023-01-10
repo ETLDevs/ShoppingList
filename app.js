f3ab0257fdbea8c0f34f995130215a34947e0986
ETLDevs <etlbdevs@gmail.com>
2023-01-10 17:18:38 +0200

    create connection & routes to server

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const { ObjectId } = require('mongodb');
+const { connectToDb, getDb } = require('./db');
+const app = express();
+let db;
+
+app.use(express.json())
+
+connectToDb((err) => {
+   if (!err){
+    app.listen(3000, () => {
+        console.log('app listenin on port 3000')
+    }); 
+    db = getDb()}
+})
+
+
+app.get('/groceries/:name', (req, res) => {
+        db.collection('groceries')
+        .findOne({name: req.params.name})
+        .then(item => {
+            res.status(200).json(item)
+        })
+        .catch(err => {
+            res.status(500).json({error: "ERROR"})
+        })
+    })
+
+app.post('/groceries', (req, res) => {
+const item = req.body
+db.collection('groceries')
+.insertOne(item)
+.then(result => {
+  res.status(201).json(result)  
+})
+.catch(err => {
+    res.status(500).json({error: "cant post your item"})
+})
+})
+
+// app.patch('/groceries/:type', (req, res) => {
+//     const updates = req.body;
+//         db.collection('groceries')
+//         .updateMany({type: req.params.type}, {$set: updates })
+//         .then(result => {
+//             res.status(200).json(result)
+//         })
+//         .catch(err => {
+//             res.status(500).json({error: "could not update the item"})
+//         })
+// })
