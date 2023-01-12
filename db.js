005e789b34fcb5e070ddda557141f26eef3d6a47
ETLDevs <etlbdevs@gmail.com>
2023-01-12 11:28:53 +0200

    all assync funcs into ES7 async

--- db.js
+++ db.js
@@ -4,16 +4,29 @@ const URI = process.env.DB_URI;
 
 let dbConnection
 
+// module.exports = {
+//     connectToDb: (cb) => {
+//         MongoClient.connect(URI)
+//         .then((client) => {
+//             dbConnection = client.db()
+//             return cb()
+//         })
+//         .catch(err => {
+//             console.log(err)
+//         })
+//     },
+//     getDb: () => dbConnection
+// }
+
 module.exports = {
-    connectToDb: (cb) => {
-        MongoClient.connect(URI)
-        .then((client) => {
-            dbConnection = client.db()
-            return cb()
-        })
-        .catch(err => {
+    connectToDb: async (cb) => {
+        try {
+        const client = await MongoClient.connect(URI);
+        dbConnection = client.db()
+        return cb()
+    } catch { err => 
             console.log(err)
-        })
+        }
     },
     getDb: () => dbConnection
 }
