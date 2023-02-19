754feae6ea8676703165da9fe4920bd488fb4622
ETLDevs <etlbdevs@gmail.com>
2023-02-19 18:09:53 +0200

    formatted docs

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,5 @@
 const navBar = document.querySelector(".navBar");
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 const search = document.querySelector(".search");
 const searchResults = document.querySelector(".searchResults");
 const allItems = document.querySelector(".allGroceries");
@@ -10,39 +10,35 @@ const choosePreference = document.querySelector(".choosePreference");
 const userPreferenceCheckboxs = document.querySelector(".userPreference");
 
 const saveItemToDb = async (id, btn) => {
-searchResults.innerHTML ='';
-search.value = ''
-  search.focus(); 
+  searchResults.innerHTML = "";
+  search.value = "";
+  search.focus();
   const result = await fetch(`http://localhost:3000/${id}`, { method: "POST" });
-const {status} = await result.json();
-if (status === 'success') {
-document.querySelectorAll(`[data-id="${id}"]`).forEach(btn => {
-    btn.disabled = 'disabled';
-  });
-}
-new Snackbar.show({
-    pos: 'bottom-left',
-    text: status === 'success' ? 'Item could not be added': 'Item could not be added',
+  const { status } = await result.json();
+  if (status === "success") {
+    document.querySelectorAll(`[data-id="${id}"]`).forEach((btn) => {
+      btn.disabled = "disabled";
+    });
+  }
+  new Snackbar.show({
+    pos: "bottom-left",
+    text:
+      status === "success"
+        ? "Item added successfully"
+        : "Item could not be added",
     showAction: false,
-    textColor: 'black',
-    backgroundColor: status === 'success' ? 'green': 'red',
+    textColor: "black",
+    backgroundColor: status === "success" ? "green" : "red",
     duration: 2000,
   });
-}
-
-
-
-
-window.onload = async () => {
-  document.querySelector('.allItems').classList.add('disabled');
 };
 
 navBar.addEventListener("click", (event) => {
   const activeList = event.target.classList.contains("activeList");
   const searchResult = event.target.classList.contains("searchResult");
   if (activeList) {
-     return location.replace("/list")
-    };
+    return location.replace("/list");
+  }
   if (searchResult) {
     saveItemToDb(event.target.dataset.id);
     searchResults.innerHTML = "";
@@ -56,20 +52,20 @@ search.addEventListener("keyup", async (event) => {
   const result = await fetch(`http://localhost:3000/${text}`);
   const data = await result.json();
   searchResults.innerHTML = "";
-let type;
+  let type;
   data.forEach((item) => {
-    const typeTitle = document.createElement('h3');
+    const typeTitle = document.createElement("h3");
     const newItem = document.createElement("li");
-    const addBtn = document.createElement('button');
+    const addBtn = document.createElement("button");
     typeTitle.innerHTML = item.type;
     newItem.innerHTML = item.name;
     newItem.dataset.id = item._id;
     newItem.classList.add("searchResult");
-    addBtn.classList.add('addItem', 'fa-solid', 'fa-plus');
+    addBtn.classList.add("addItem", "fa-solid", "fa-plus");
     addBtn.dataset.id = item._id;
-    if(item.onList) addBtn.disabled = 'disabled';
+    if (item.onList) addBtn.disabled = "disabled";
     newItem.appendChild(addBtn);
-    if(typeTitle.innerHTML === type) return searchResults.append(newItem)
+    if (typeTitle.innerHTML === type) return searchResults.append(newItem);
     searchResults.append(typeTitle, newItem);
     type = item.type;
   });
